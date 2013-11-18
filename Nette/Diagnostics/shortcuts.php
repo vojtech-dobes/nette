576017ac58f5b24d5b255e34c7844890ef778f4f
David Grudl <david@grudl.com>
2013-11-18 13:44:06 +0100

    Revert "Debugger: added shortcut debug() and dlog()"
    This reverts commit 1bc4260a7e8d2b13dffb594395326169ec7551df.

--- Nette/Diagnostics/shortcuts.php
+++ Nette/Diagnostics/shortcuts.php
@@ -22,18 +22,3 @@ function dump($var)
 	}
 	return $var;
 }
-
-
-/**
- * Nette\Diagnostics\Debugger::log() shortcut.
- */
-function dlog($var = NULL)
-{
-	if (func_num_args() === 0) {
-		Debugger::log(new Exception, 'dlog');
-	}
-	foreach (func_get_args() as $arg) {
-		Debugger::log($arg, 'dlog');
-	}
-	return $var;
-}
